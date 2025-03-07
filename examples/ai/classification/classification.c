1bc4c676ebc02ec9b479341741baa950d163c12c
Antoniodv <antonio.delvecchio2@studio.unibo.it>
2025-03-07 15:53:36 +0100

    uart init

--- examples/ai/classification/classification.c
+++ examples/ai/classification/classification.c
@@ -303,10 +303,25 @@ void init_monitor(void){
 int main(void){
     /* Init HW */
     pi_bsp_init();
+    
+    pi_freq_set(PI_FREQ_DOMAIN_FC, FREQ_FC*1000*1000);
+    __pi_pmu_voltage_set(PI_PMU_DOMAIN_FC, 1200);
+    pi_open_from_conf(&device, &uart_conf);
+    if (pi_uart_open(&device))
+    {
+      printf("[UART] open failed !\n");
+      pmsis_exit(-1);
+    }
+
     cpxInit();
     cpxEnableFunction(CPX_F_WIFI_CTRL);
     cpxPrintToConsole(LOG_TO_CRTP, "*** Classification with Performance Monitoring ***\n");
-    
+
+    // For debugging
+    struct pi_uart_conf uart_conf;
+    struct pi_device device;
+    pi_uart_conf_init(&uart_conf);
+    uart_conf.baudrate_bps = 115200;
     while(1){
       cpxPrintToConsole(LOG_TO_CRTP, "main\n");
     }
